510cb09b1315816b259b2a2130631ee58103ac97
Huseyn <59554521+huseynyusif@users.noreply.github.com>
2024-05-05 17:55:55 +0400

    finished

--- Main.java
+++ Main.java
@@ -1,11 +1,5 @@
 package org.example;
 
-import jdbc.Update;
-
-import java.sql.*;
-import java.time.LocalDate;
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
 
